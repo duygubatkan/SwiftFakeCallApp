d411b9d96b76335af1ebb1ef6339e0c0fc8ad669
Apple <duygubatkan@gmail.com>
2017-10-01 23:41:48 +0300

    * tableView added to homePageVC

--- FakeCall/HomePageViewController.swift
+++ FakeCall/HomePageViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class HomePageViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
